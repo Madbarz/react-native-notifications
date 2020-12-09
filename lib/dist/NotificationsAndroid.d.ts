fff23b9f87681b3f621273568ba221a60f44e4d2
Nikola Mratinic <nikola.mratinic@gmail.com>
2020-12-09 16:08:48 +0100

    added lib/dist folder

---
+++ lib/dist/NotificationsAndroid.d.ts
@@ -0,0 +1,15 @@
+import 'proxy-polyfill';
+import { Commands } from './commands/Commands';
+import { NotificationChannel } from './interfaces/NotificationChannel';
+export declare class NotificationsAndroid {
+    private readonly commands;
+    constructor(commands: Commands);
+    /**
+    * Refresh FCM token
+    */
+    registerRemoteNotifications(): void;
+    /**
+     * setNotificationChannel
+     */
+    setNotificationChannel(notificationChannel: NotificationChannel): void;
+}
